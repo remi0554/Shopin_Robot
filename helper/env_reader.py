f037c9f20d6a40fb7719b5166207717ffae07f93
remi0554 <118734955+remi0554@users.noreply.github.com>
2026-05-19 12:33:31 +0000

    Browser ajustemet 6

--- helper/env_reader.py
+++ helper/env_reader.py
@@ -1,7 +1,16 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 def get_env(var):
-    return os.getenv(var)
+    value = os.getenv(var)
+    if not value:
+        raise ValueError(f"Environment variable '{var}' not found")
+    return value
+
+
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
+# def get_env(var):
+#     return os.getenv(var)
